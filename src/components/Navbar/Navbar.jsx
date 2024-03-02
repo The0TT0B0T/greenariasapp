af1ecba3a6379e44ba0fefdbaf159a6d66fd51bc
The0TT0B0T <120422116+The0TT0B0T@users.noreply.github.com>
2024-03-01 22:42:37 -0600

    @media queries

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,29 @@
-import React, { useContext, useState } from 'react'
-import './Navbar.css'
-import logo from "../Assets/Green-Aria's-logo2.png"
-import cart_icon from '../Assets/cart_icon.png'
-import { Link } from 'react-router-dom'
-import { ShopContext } from '../../context/ShopContext'
+import React, { useContext, useRef, useState } from 'react';
+import './Navbar.css';
+import logo from "../Assets/Green-Aria's-logo2.png";
+import cart_icon from '../Assets/cart_icon.png';
+import { Link } from 'react-router-dom';
+import { ShopContext } from '../../context/ShopContext';
+import nav_dropdown from '../Assets/dropdown_icon.png';
 
 export const Navbar = () => {
 
-    const [menu, setMenu] =useState("home")
-    const {getTotalCartItems} = useContext(ShopContext)
+    const [menu, setMenu] =useState("home");
+    const {getTotalCartItems} = useContext(ShopContext);
+    const menuRef = useRef();
+
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle("nav-menu-visible");
+        e.target.classList.toggle('open');
+    }
 
   return (
     <div className='navbar'>
         <div className="nav-logo">
         <Link style={{textDecoration: 'none'}} to="/"><img src={logo} alt="" onClick={()=>{setMenu("home")}} to="/home"/></Link>
-            
-            <p></p>
         </div>
-        <ul className="nav-menu">
+        <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+        <ul ref={menuRef} className="nav-menu">
             <li onClick={()=>{setMenu("home")}}><Link style={{textDecoration: 'none'}} to="/">Home</Link>{menu === "home" ? <hr /> : <></>}</li>
             <li onClick={()=>{setMenu("landscaping")}}><Link style={{textDecoration: 'none'}}  to="/landscaping">Landscaping</Link>{menu === "landscaping" ? <hr /> : <></>}</li>
             <li onClick={()=>{setMenu("snowRemoval")}}><Link style={{textDecoration: 'none'}}  to="/snowRemoval">Snow Removal</Link>{menu === "snowRemoval" ? <hr /> : <></>}</li>
